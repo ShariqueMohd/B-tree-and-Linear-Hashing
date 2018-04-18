82c2be29cb92146d90ebdcb75ec451c2e6bd90fa
Sharique Mohd <1sharique1@gmail.com>
2018-04-18 11:07:44 +0530

    Linear Hashing Implementation with Sample Example

---
+++ Qtwo.cpp
@@ -0,0 +1,27 @@
+#include "LinearHashing.hpp"
+
+using namespace std;
+
+unsigned long hashFunction(unsigned long value, const int& key) {
+    return (key & ((1 << (value + 1)) - 1));
+}
+
+int main(int argc, char* argv[]) {
+    string filename = argv[1];
+    int M = atoi(argv[2]);
+    int B = atoi(argv[3]);
+
+    ifstream inputFile;
+    inputFile.open(filename);
+    int num;
+    
+    Hash<int> h(B / 4, hashFunction);
+    
+    while (inputFile >> num) {
+        if (!h.Find(num)) {
+            cout << num << endl;
+            h.Insert(num);
+        }
+    }
+    return 0;
+}
